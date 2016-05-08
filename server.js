190011eb8268606acf110c442ff0390789dda98c
Marcus <marcus@lastcraft.com>
2016-05-08 14:41:22 +0000

    Allowing manual override of server port

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+var commandLineArgs = require('command-line-args');
 var express = require('express');
-var server = express();
 
+var arguments = commandLineArgs([
+                    { name: "help", type: Boolean, alias: "h" },
+                    { name: "port", type: Number, alias: "p", default: 80 }]);
+if (arguments.parse()["help"]) {
+    console.log(arguments.getUsage());
+    process.exit();
+}
+var port = arguments.parse()["port"]
+var server = express();
 server.use('/', express.static(__dirname + '/public'));
 
-server.listen(80, function () {
-  console.log('Started on port 80');
+server.listen(port, function () {
+  console.log('Started on port ' + port);
 });
